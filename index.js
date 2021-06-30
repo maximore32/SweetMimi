95768f053aef3db95226c97d1361ffa53b79865f
maximore32 <78425799+maximore32@users.noreply.github.com>
2021-06-29 22:55:35 -0300

    camb1

--- index.js
+++ index.js
@@ -14,16 +14,60 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL =process.env.SONIURL  || "mongodb+srv://maximusbrain:masterit@cluster0.6di0t.mongodb.net/testsoni?retryWrites=true&w=majority";
+const MONGO_URL ="mongodb+srv://maximusbrain:masterit@cluster0.6di0t.mongodb.net/testsoni?retryWrites=true&w=majority";
 
 app.get('/', (req, res)=>{	  
-  MongoClient.connect(MONGO_URL,{ useUnifiedTopology: true }, (err, db) => {    
-  const dbo = db.db("testsoni");  
-  dbo.collection("dulce").find().toArray(function(err, dulces) {	      
-      res.render('home.html',{data:dulces});
-	}); 
+  MongoClient.connect(MONGO_URL,{ useUnifiedTopology: true }, (err, db) => {  
+  const dbo = db.db("testsoni"); 
+  const data = [];   
+  // Consultamos los platos
+  dbo.collection("dulce").find().toArray()
+  .then((dataplatos) => { 
+// en data[0] quedan los platos
+    data.push(dataplatos)
+  }) 
+  .then(() => {
+  //Consultamos en la base las categorías
+    dbo.collection("categorias").find().toArray()
+    .then((datacategorias) => { 
+// en data[1] quedan los categorias
+      data.push(datacategorias)      
+      res.render('home.html',{data:data});      
+    }) 
+  })
+});
+});
+
+
+// Ruta individual del plato
+app.get('/plato/:id', (req, res)=>{	  
+  MongoClient.connect(MONGO_URL,{ useUnifiedTopology: true }, (err, db) => {  
+  const dbo = db.db("testsoni"); 
+  // Obtenemos la id de la URL y la convertimos a entero
+  var id = parseInt(req.params.id);     
+  // Buscamos en la base con un findeOne el plato
+  dbo.collection("dulce").findOne({"id":id},function(err, data) {   	
+    if (data){     
+          res.status(200).render('producto.html',{id:data.id,plato:data.Nombre,img:data.Imagen,descripcion:data.Descripción,categoria:data.Categoria,precio:data.Precio}
+          );	
+      }else{
+          res.status(404).send(`<p>ERROR</p>`)
+
+      }    		
+    });
 });	
 });
+
+app.get('/categoria/:cat', (req, res)=>{	  
+  MongoClient.connect(MONGO_URL,{ useUnifiedTopology: true }, (err, db) => {  
+      const dbo = db.db("testsoni");         
+      // Buscamos en platos y filtramos por su categoría para mostrarlos
+      dbo.collection("dulce").find({"Categoria":req.params.cat}).toArray()
+          .then((data) => {      
+          res.render('categoria.html',{data:data,categoria:req.params.cat});
+          }) 
+      })
+      });
 
 
 app.listen(port);
