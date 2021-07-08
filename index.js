0a91da0c156b9b8dccc1fbda2a5a42adf45dbfe5
maximore32 <78425799+maximore32@users.noreply.github.com>
2021-07-08 14:45:15 -0300

    fusion

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ var	app = express();
 app.use(express.static('public'));
 const port = process.env.PORT || 8080;
 
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+app.use("/public/img", express.static('img'));
+
+var fs = require('fs');
+const dir = __dirname +'/public/img/'; //en lugar de user sería el nombre de usuario si estuviera logueado
+
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
 nunjucks.configure('views', {
   autoescape: true,
   express: app
@@ -14,7 +25,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const MongoClient = require('mongodb').MongoClient;
-const MONGO_URL = process.env.SONIURL;
+const MONGO_URL = process.env.SONIURL|| "mongodb+srv://maximusbrain:defensa143@cluster0.6di0t.mongodb.net/testsoni?retryWrites=true&w=majority";
 
 app.get('/', (req, res)=>{	  
   MongoClient.connect(MONGO_URL,{ useUnifiedTopology: true }, (err, db) => {  
@@ -103,5 +114,131 @@ app.get('/categoria/:cat', (req, res)=>{
   
 });
 
+//----------------------------------Secret-Login-------------------------------------------------------------------------------------//
+
+session = require('express-session');
+app.use(session({
+    secret: 'string-supersecreto320',
+    name: 'sessionId',
+    proxy: true,
+    resave: true,
+    saveUninitialized: true ,
+    cookie: { maxAge:  60 * 60 * 1000 }  
+}));
+
+var auth = function(req, res, next) {
+    if (req.session.login)
+      return next();
+    else
+      return res.render("noacces.html");
+  };
+
+  //Index para el login
+app.get("/admin", (req, res) => {
+    res.render("index2.html");
+});
+
+// Login endpoint
+app.all('/login', function (req, res) {
+    if (!req.body.user || !req.body.pass) {
+      res.render('noacces.html');    
+    } else {
+  
+      MongoClient.connect(MONGO_URL,{ useUnifiedTopology: true }, (err, db) => {  
+          const dbo = db.db("testsoni");  
+          dbo.collection("login").findOne({$and:[{"user":req.body.user},{"pass":req.body.pass}]},function(err, data) {             
+              //console.log(data); 
+              if(data){
+                  req.session.login = true;  
+                  req.session.nombre = data.user;               
+                  res.render("bienlogeado.html");  
+              }
+              else{
+                res.render('noacces.html');
+              } 
+            })
+          });
+        }
+      });
+
+      // Logout endpoint
+app.get('/logout', function (req, res) {
+    req.session.destroy();
+    res.send("Sesión cerrada!");
+  });
+   
+  // Get content endpoint
+  app.get('/content', auth, function (req, res) {    
+      res.render('formudulces.html');        
+  });
+
+  app.get('/categorias', auth, function (req, res) {    
+    res.render('agregarcategoria.html');     
+});
+    
+  
+  // Recibimos la información del formulario de alta de platos e insertamos en la base de datos
+  app.post('/altadulce', (req, res)=>{
+      MongoClient.connect(MONGO_URL,{ useUnifiedTopology: true }, (err, db) => {  
+      const dbo = db.db("testsoni")
+      // key de la base datos : req.body.name_campo_formulario
+      dbo.collection("dulce").insertOne(
+          {   
+              id:parseInt(req.body.id),
+              Nombre: req.body.nombre,            
+              Descripción:req.body.descripcion,
+              Precio: req.body.precio,
+              Categoria:req.body.categoria,
+              Imagen:req.files.foto.name
+  
+              
+          },
+          function (err, res) {
+              if (err) {
+              db.close();
+              return console.log(err);
+              }
+              db.close()
+          })
+          if (!req.files)
+          return res.status(400).send('No files were uploaded.');
+       
+        
+        let foto = req.files.foto;
+        console.log(foto)
+       
+        if (!fs.existsSync(dir)){
+          fs.mkdirSync(dir);
+        }
+        //foto.mv(__dirname + '/img/'+ req.files.foto.name, function(err) {
+        foto.mv(__dirname + '/public/img/'+ req.files.foto.name.jpg, function(err) {
+          if (err)
+            return res.status(500).send(err); 
+            res.send('<p>Agregado exitosamente</p><p><a href="/content">Agregar Dulce</a></p><p><a href="/logout">Cerrar Sesión</a></p>'+ req.files.foto.name);
+        });
+          
+      })
+  })
+
+  // Recibimos la información del formulario de alta de categorias e insertamos en la base de datos
+app.post('/altacategoria', (req, res)=>{
+  MongoClient.connect(MONGO_URL,{ useUnifiedTopology: true }, (err, db) => {  
+  const dbo = db.db("testsoni")
+  // key de la base datos : req.body.name_campo_formulario
+  dbo.collection("categorias").insertOne(
+      {   
+          categoria : req.body.categoria,
+          id: req.body.id
+      },
+      function (err, res) {
+          if (err) {
+          db.close();
+          return console.log(err);
+          }
+          db.close()
+      })
+      res.send('<p>Categoria agregada exitosamente</p><p><a href="/categorias">Agregas otra categoria</a></p><p><a href="/content">Agregar Dulce</a></p><p><a href="/logout">Cerrar Sesión</a></p>')
+  })
+})
 
 app.listen(port);
